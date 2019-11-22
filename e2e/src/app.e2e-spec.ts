1cae530c0b0e1f77cdfce2d3b7819fb561446dff
Ayo <ramon.aycojr@gmail.com>
2019-11-22 11:13:06 +0800

    FH Books

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to coffee-hub!');
+    expect(page.getTitleText()).toEqual('Welcome to FH Books!');
   });
 
   afterEach(async () => {
